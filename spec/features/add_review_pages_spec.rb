a583bc55e14d701c2bcad0e4add9f286990b1c4a
James Higgins <james.milan.higgins@gmail.com>
2017-06-30 09:24:35 -0700

    add spec for add review

---
+++ spec/features/add_review_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "the  add review process" do
+  it "adds a review" do
+    product = Product.create(:name => 'James', :cost => '5', :country => "USA")
+    review = Review.create(:author => 'Mike',:content_body => 'test text', :rating => '5', :product_id => product.id)
+    visit product_path(product)
+    click_on 'Add A Review'
+    fill_in 'Author', :with => 'Updated James'
+    fill_in 'Content body', :with => 'Updated test text'
+    fill_in 'Rating', :with => '5'
+    click_on 'Create Review'
+    expect(page).to have_content 'Updated James'
+  end
+
+  it "gives error when no description is entered" do
+    product = Product.create(:name => 'James', :cost => '5', :country => "USA")
+    review = Review.create(:author => 'Mike',:content_body => 'test text', :rating => '5', :product_id => product.id)
+    visit product_path(product)
+    click_on 'Add A Review'
+    fill_in 'Author', :with => ''
+    click_button 'Create Review'
+    expect(page).to have_content 'errors'
+  end
+end
