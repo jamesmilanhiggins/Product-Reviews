b7ac89c10c462b90df5b25cfb4afaa9f18ad625d
James Higgins <james.milan.higgins@gmail.com>
2017-06-30 09:02:13 -0700

    Add full CRUD for both product and review

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,51 @@
 class ProductsController < ApplicationController
+  
   def index
     @products = Product.all
-
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to  products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    reviews = @product.reviews
+    reviews.each do |review|
+      review.destroy
+    end
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :cost, :country)
+    end
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,43 @@
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@review.product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating, :product_id)
+    end
+end
